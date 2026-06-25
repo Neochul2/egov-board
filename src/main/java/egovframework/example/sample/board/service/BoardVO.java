3e4b05d16ebd73c99555598d7fedaf4201ad472f
jchul <jik4489@gamil.com>
2026-06-25 18:20:14 +0900

    feat: 메인화면 완성 - BoardVO 추가

--- src/main/java/egovframework/example/sample/board/service/BoardVO.java
+++ src/main/java/egovframework/example/sample/board/service/BoardVO.java
@@ -1,5 +1,24 @@
 package egovframework.example.sample.board.service;
 
-public class BoardVO {
+import egovframework.example.sample.service.SampleDefaultVO;
+import lombok.Data;
+import lombok.ToString;
 
+@Data
+@ToString
+public class BoardVO extends SampleDefaultVO {
+    private String userId;
+    private String userNm;
+    private String password;
+    private String useYn;
+    private String idx;
+    private String title;
+    private String contents;
+    private String count;
+    private String writer;
+    private String writerNm;
+    private String indate;
+    private String seq;
+    private String reply;
+    private String filename;
 }
